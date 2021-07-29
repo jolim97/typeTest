73b9d2eee0b09b31f9425540985cb7b6de9e04f9
Jongwon <ljw1015@hanyang.ac.kr>
2021-07-30 01:27:19 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,93 @@
+// variables
+const GAME_TIME = 9;
+let score = 0;
+let time = GAME_TIME;
+let isPlaying = false;
+let timeInterval;
+let checkInterval;
+let words = [];
+
+const wordInput = document.querySelector(".word-input");
+const wordDisplay = document.querySelector(".word-display");
+const scoreDisplay = document.querySelector(".score");
+const timeDisplay = document.querySelector(".time");
+const button = document.querySelector(".button");
+
+
+init();
+
+function init(){
+    buttonChange("Loading Game...");
+    getWords();
+    wordInput.addEventListener("input",checkMatch)
+}
+function run(){
+    if(isPlaying){
+        return;
+    }
+    isPlaying = true;
+    score = 0;
+    time = GAME_TIME;
+    wordInput.focus();
+    scoreDisplay.innerText = 0;
+    timeInterval = setInterval(countDown, 1000);
+    checkInterval = setInterval(checkStatus, 50);
+    buttonChange('Now gaming ...')
+}
+
+function checkStatus(){
+    if(!isPlaying && time === 0){
+        buttonChange("Start")
+        clearInterval(checkInterval)
+    }
+}
+
+function getWords(){
+    axios.get('https://random-word-api.herokuapp.com/word?number=10000')
+        .then(function (response) {
+        // handle success
+        response.data.forEach((word)=>{
+            if(word.length < 10){
+                words.push(word)
+            }
+        })
+        buttonChange("Start")
+        console.log(response);
+    })
+        .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+        .then(function () {
+        // always executed
+    });
+    buttonChange("Start")
+}
+
+function checkMatch(){
+    if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()){
+        wordInput.value = "";
+        if(!isPlaying){
+            return;
+        }
+        score++;
+        scoreDisplay.innerText = score;
+        time = GAME_TIME;
+        const randomIndex = Math.floor(Math.random() * words.length);
+        wordDisplay.innerText = words[randomIndex]
+    }
+}
+
+
+function countDown(){
+    time > 0 ? time-- : isPlaying = false;
+    if(!isPlaying){
+        clearInterval(timeInterval)
+    }
+    timeDisplay.innerText = time;
+}
+
+function buttonChange(text){
+    button.innerText = text;
+    text === "Start" ? button.classList.remove("loading") : button.classList.add("loading");
+}
